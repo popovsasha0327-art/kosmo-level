e981c4f35d26564a6803be7fd4214bf50f813eef
Oleksandr <popovsasha0327@gmail.com>
2026-04-23 19:06:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
+const CACHE_NAME = 'lmsh-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+// Установка и кэширование
 self.addEventListener('install', (e) => {
-  console.log('SW установлен');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
 });
+
+// Работа в офлайне
 self.addEventListener('fetch', (e) => {
-  return; 
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
